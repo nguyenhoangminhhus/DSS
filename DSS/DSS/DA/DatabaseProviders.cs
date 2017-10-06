314e8aec26da2c627ce02048b3da19edd3cf1601
Vuong_Thong <thongvx@bloomgoo.vn>
2017-10-06 22:22:11 +0700

    init

---
+++ DSS/DSS/DA/DatabaseProviders.cs
@@ -0,0 +1,48 @@
+﻿using DSS.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DSS.DA
+{
+
+
+    public class DatabaseProviders
+    {
+        private string conString = "Data Source=DESKTOP-O51905Q\\SQLEXPRESS;Initial Catalog=dss;Integrated Security=True";
+
+        public void add(Person person)
+        {
+            DataTable data = new DataTable();
+            using (SqlConnection con = new SqlConnection(conString))
+            {
+                con.Open();
+                DataTable dt = new DataTable();
+                SqlDataAdapter da = new SqlDataAdapter("Select * from Table_Teacher", con);
+                da.Fill(dt);
+                DataRow r = dt.NewRow();
+
+                r[0] = DBNull.Value;
+                r[1] = person.name;
+                r[2] = person.sexual;
+                r[3] = person.dateofbirth;
+                r[4] = person.homtown;
+                r[5] = person.position;
+                r[6] = person.image;
+                r[7] = person.description;
+
+                dt.Rows.Add(r);
+                SqlCommandBuilder cb = new SqlCommandBuilder(da);
+                da.Update(dt);
+                con.Close();
+                MessageBox.Show("Lưu lại thành công");
+            }
+        }
+
+    }
+}
---
+++ DSS/DSS/Models/Person.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSS.Models
+{
+    public class Person
+    {
+        public string name;
+        public string sexual;
+        public string dateofbirth;
+        public string homtown;
+        public string position;
+        public byte[] image;
+        public string description;
+
+        public Person()
+        {
+        }
+
+        public Person(string name, string sexual, string dateofbirth, string homtown, string position, byte[] image, string description)
+        {
+            this.name = name;
+            this.sexual = sexual;
+            this.dateofbirth = dateofbirth;
+            this.homtown = homtown;
+            this.position = position;
+            this.image = image;
+            this.description = description;
+        }
+        
+    }
+}
